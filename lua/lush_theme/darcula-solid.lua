3db4f697ae47d9538603fe787636d617d499700c
Brian Pooe <bria.method@gmail.com>
2023-12-17 21:26:45 +0200

    fix: update functions and local member variables

--- lua/lush_theme/darcula-solid.lua
+++ lua/lush_theme/darcula-solid.lua
@@ -299,7 +299,9 @@ return lush(function(injected_functions)
 		sym("@lsp.mod.declaration.typescript")({ fg = purple }),
 		sym("@lsp.typemod.class.defaultLibrary.typescript")({ fg = yellow }),
 		sym("@lsp.type.namespace.typescript")({ fg = white }),
+		sym("@lsp.typemod.member.declaration.typescript")({ fg = yellow }),
 		sym("@lsp.typemod.parameter.declaration.typescript")({ fg = white }),
+		sym("@lsp.typemod.variable.declaration.typescript ")({ fg = white }),
 
 		sym("@label.json")({ fg = purple }),
 		sym("@label.jsonc")({ fg = purple }),
