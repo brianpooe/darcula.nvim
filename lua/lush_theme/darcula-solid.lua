3723dd9d4546cbc5c80d0b98f5c9bfcf3e837931
Brian Pooe <bria.method@gmail.com>
2023-12-17 11:02:42 +0200

    fix: override of styles

--- lua/lush_theme/darcula-solid.lua
+++ lua/lush_theme/darcula-solid.lua
@@ -230,6 +230,7 @@ return lush(function(injected_functions)
 		sym("@parameter")({ fg = fg }),
 		sym("@parameter.reference")({ fg = fg }),
 		sym("@variable")({ fg = white }), -- Any variable name that does not have another highlight
+		sym("@variable.typescript")({ fg = purple }), -- Any variable name that does not have another highlight
 		sym("@variable.builtin")({ Constant, gui = it }), -- Variable names that are defined by the languages like `this` or `self`.
 
 		sym("@function")({ Function }),
@@ -254,6 +255,7 @@ return lush(function(injected_functions)
 		sym("@include.typescript")({ fg = orange }), -- includes: `#include` in C `use` or `extern crate` in Rust or `require` in Lua.
 
 		sym("@type")({ fg = white }),
+		sym("@type.typescript")({ fg = purple }),
 		sym("@type.builtin")({ Type, gui = it }),
 
 		sym("@punctuation.delimiter")({ Delimiter }), -- delimiters ie: `.`
